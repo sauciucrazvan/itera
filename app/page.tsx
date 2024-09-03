f59676a846e9b04631cf6c2b4259628c5f82cc47
Razvan <razvansauciucc@outlook.com>
2024-09-03 16:45:11 +0300

    Added DaisyUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      Hello world!
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
 }
