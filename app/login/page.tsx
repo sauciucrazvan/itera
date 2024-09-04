3f712f55a491e0edba5f115277c26af3b0db254c
Razvan <razvansauciucc@outlook.com>
2024-09-04 15:46:47 +0300

    Designed the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,21 @@
+import { FaGoogle } from "react-icons/fa6";
+import Footer from "../(components)/Footer";
+
 export default function Login() {
   return (
     <>
-      <div>Login</div>
+      <section className="h-[75vh] flex flex-col justify-center items-center gap-1">
+        <div className="text-base font-bold">Login into your account</div>
+        <div className="text-sm">Please use one of the providers below</div>
+        <div className="w-[20vw]">
+          <div className="divider" />
+        </div>
+        <button className="btn btn-outline">
+          <FaGoogle /> Login with Google
+        </button>
+      </section>
+      <div className="divider" />
+      <Footer />
     </>
   );
 }
