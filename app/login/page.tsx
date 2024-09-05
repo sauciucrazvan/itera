51c1cc523883cc4b2ad7145d64539aa701f74486
Razvan <razvansauciucc@outlook.com>
2024-09-05 16:41:14 +0300

    Added breadcrumbs to different pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,30 @@
 import { FaGoogle } from "react-icons/fa6";
 import Footer from "../(components)/structure/Footer";
+import Link from "next/link";
 
 export default function Login() {
   return (
     <>
-      <section className="h-[75vh] flex flex-col justify-center items-center gap-1">
-        <div className="text-base font-bold">Login into your account</div>
-        <div className="text-sm">Please use one of the providers below</div>
-        <div className="w-[20vw]">
-          <div className="divider" />
+      <section className="py-2 px-4">
+        <div className="breadcrumbs text-sm">
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>Login</li>
+          </ul>
         </div>
-        <button className="btn btn-outline">
-          <FaGoogle /> Login with Google
-        </button>
+        <section className="h-[75vh] flex flex-col justify-center items-center gap-1">
+          <div className="text-base font-bold">Login into your account</div>
+          <div className="text-sm">Please use one of the providers below</div>
+          <div className="w-[20vw]">
+            <div className="divider" />
+          </div>
+          <button className="btn btn-outline">
+            <FaGoogle /> Login with Google
+          </button>
+        </section>
       </section>
-      <div className="divider" />
-      <Footer />
     </>
   );
 }
--- app/thread/[id]/page.tsx
+++ app/thread/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 
 export default function ViewIssue() {
@@ -6,7 +7,29 @@ export default function ViewIssue() {
 
   return (
     <>
-      <div>Viewing issue #{id}</div>
+      <section className="py-2 px-4">
+        <div className="breadcrumbs text-sm">
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/">Issues</Link>
+            </li>
+            <li>Viewing Thread</li>
+          </ul>
+        </div>
+        <section className="artboard bg-base-200 px-4 py-2 rounded-md">
+          <h1 className="font-bold text-lg">Thread #{id}</h1>
+          <div className="divider m-0" />
+          <section className="pt-2">
+            <h1>Title</h1>
+          </section>
+          <section className="py-2">
+            <h1>Content</h1>
+          </section>
+        </section>
+      </section>
     </>
   );
 }
