aa1db5bdec2fb72a8086d7a395edd6fe6ea76ba9
Razvan <razvansauciucc@outlook.com>
2024-09-05 16:46:10 +0300

    Completed the thread viewing page layout

--- app/thread/[id]/page.tsx
+++ app/thread/[id]/page.tsx
@@ -19,16 +19,25 @@ export default function ViewIssue() {
             <li>Viewing Thread</li>
           </ul>
         </div>
-        <section className="artboard bg-base-200 px-4 py-2 rounded-md">
-          <h1 className="font-bold text-lg">Thread #{id}</h1>
-          <div className="divider m-0" />
-          <section className="pt-2">
-            <h1>Title</h1>
+        <div className="flex flex-col lg:flex-row gap-2">
+          <section className="artboard bg-base-200 px-4 py-2 rounded-md lg:w-[60vw]">
+            <h1 className="font-bold text-lg">Thread #{id}</h1>
+            <div className="divider m-0" />
+            <section className="pt-2">
+              <h1>Title</h1>
+            </section>
+            <section className="py-2">
+              <h1>Content (Description, author and creation date)</h1>
+            </section>
           </section>
-          <section className="py-2">
-            <h1>Content</h1>
+          <section className="artboard bg-base-200 px-4 py-2 rounded-md">
+            <h1 className="font-bold text-lg">Comments</h1>
+            <div className="divider m-0" />
+            <section className="pt-2">
+              <h1>Title</h1>
+            </section>
           </section>
-        </section>
+        </div>
       </section>
     </>
   );
