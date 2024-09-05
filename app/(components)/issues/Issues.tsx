e6786afc2cfa06987f307b227fefce00d6ba8aa1
Razvan <razvansauciucc@outlook.com>
2024-09-05 15:31:51 +0300

    Added severities to issues

--- app/(components)/issues/Issues.tsx
+++ app/(components)/issues/Issues.tsx
@@ -3,16 +3,59 @@ import StatusBadge from "./StatusBadge";
 import { FaPlus } from "react-icons/fa6";
 
 const issues = [
-  { id: 9, title: "user account deleted for no reason", status: "open" },
-  { id: 8, title: "Placeholder data on the dashboard page", status: "open" },
-  { id: 7, title: "header buttons no workie", status: "open" },
-  { id: 3, title: "New issue button does nothing", status: "open" },
-  { id: 5, title: "Responsiveness bug", status: "reviewing" },
-  { id: 2, title: "Padding issue", status: "reviewing" },
-  { id: 6, title: "Broken UI component", status: "closed" },
-  { id: 4, title: "Wrong redirect", status: "closed" },
-  { id: 1, title: "Data not available", status: "closed" },
+  {
+    id: 9,
+    title: "user account deleted for no reason",
+    status: "open",
+    severity: "critical",
+  },
+  {
+    id: 8,
+    title: "Placeholder data on the dashboard page",
+    status: "open",
+    severity: "minor",
+  },
+  {
+    id: 7,
+    title: "header buttons no workie",
+    status: "open",
+    severity: "medium",
+  },
+  {
+    id: 3,
+    title: "New issue button does nothing",
+    status: "open",
+    severity: "major",
+  },
+  {
+    id: 5,
+    title: "Responsiveness bug",
+    status: "reviewing",
+    severity: "minor",
+  },
+  { id: 2, title: "Padding issue", status: "reviewing", severity: "minor" },
+  { id: 6, title: "Broken UI component", status: "closed", severity: "major" },
+  { id: 4, title: "Wrong redirect", status: "closed", severity: "medium" },
+  {
+    id: 1,
+    title: "Data not available",
+    status: "closed",
+    severity: "critical",
+  },
 ];
+
+const badgeTypes: { [key: string]: string } = {
+  open: "badge-success",
+  closed: "badge-error",
+  reviewing: "badge-secondary",
+};
+
+const severityTypes: { [key: string]: string } = {
+  minor: "text-success",
+  medium: "text-secondary",
+  major: "text-warning",
+  critical: "text-error",
+};
 
 export default function Issues() {
   return (
@@ -33,6 +76,7 @@ export default function Issues() {
             <tr>
               <th className="hidden lg:block">ID</th>
               <th>Title</th>
+              <th>Severity</th>
               <th>Status</th>
               <th>Actions</th>
             </tr>
@@ -43,7 +87,18 @@ export default function Issues() {
                 <th className="hidden lg:block">{issue.id}</th>
                 <td>{issue.title}</td>
                 <td>
-                  <StatusBadge type={issue.status} />
+                  <div
+                    className={
+                      "badge badge-ghost " + severityTypes[issue.severity]
+                    }
+                  >
+                    {issue.severity}
+                  </div>
+                </td>
+                <td>
+                  <div className={"badge " + badgeTypes[issue.status]}>
+                    {issue.status}
+                  </div>
                 </td>
                 <td>
                   <Link href={"/thread/" + issue.id}>View</Link>
