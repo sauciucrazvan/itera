02d0226f1d5feaaeb6e95446dbf4ef1ad89403e3
Razvan <razvansauciucc@outlook.com>
2024-09-07 20:13:05 +0300

    Added pagination to the Issues on the dashboard

--- app/(components)/issues/subcomponents/IssuesBody.tsx
+++ app/(components)/issues/subcomponents/IssuesBody.tsx
@@ -1,33 +1,43 @@
 import IssueRow from "./IssueRow";
 import Loading from "../../Loading";
 import Error from "../../Error";
-
-import { useEffect, useState } from "react";
-
+import { useEffect, useState, useMemo } from "react";
 import { severityRank } from "@/app/(types)/Severities";
 import { statusRank } from "@/app/(types)/Statuses";
 import { getThreads, IssueThread } from "@/app/(database)/getThreads";
 
 export default function IssuesBody() {
+  const ITEMS_PER_PAGE = 10;
+
   const [data, setData] = useState<IssueThread[]>([]),
     [loading, setLoading] = useState(true),
-    [error, setError] = useState(false);
+    [error, setError] = useState(false),
+    [page, setPage] = useState(1);
+
+  const displayData = useMemo(() => {
+    const startIndex = (page - 1) * ITEMS_PER_PAGE;
+    return data.slice(startIndex, startIndex + ITEMS_PER_PAGE);
+  }, [data, page]);
+
+  function handleNavigation(increment: boolean) {
+    setPage((prevPage) => Math.max(increment ? prevPage + 1 : prevPage - 1, 1));
+  }
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const issuesData: IssueThread[] = await getThreads();
+        setLoading(true);
 
+        const issuesData: IssueThread[] = await getThreads();
         issuesData.sort((a, b) => {
           if (statusRank[a.status] !== statusRank[b.status]) {
             return statusRank[a.status] - statusRank[b.status];
           }
-
           return severityRank[a.severity] - severityRank[b.severity];
         });
 
         setData(issuesData);
-      } catch (error) {
+      } catch (err) {
         setError(true);
       } finally {
         setLoading(false);
@@ -40,7 +50,7 @@ export default function IssuesBody() {
   if (loading) return <Loading />;
   if (error) return <Error />;
 
-  if (!Array.isArray(data) || data.length === 0) {
+  if (data.length === 0) {
     return (
       <div className="text-sm py-4 px-2">No entries found in the database.</div>
     );
@@ -50,7 +60,7 @@ export default function IssuesBody() {
     <>
       <section>
         <div className="overflow-x-auto">
-          <table className="table table-zebra-zebra">
+          <table className="table table-zebra">
             <thead>
               <tr>
                 <th>Title</th>
@@ -59,20 +69,28 @@ export default function IssuesBody() {
               </tr>
             </thead>
             <tbody>
-              {data.map((issue: IssueThread) => (
+              {displayData.map((issue: IssueThread) => (
                 <IssueRow key={issue.id} issue={issue} />
               ))}
             </tbody>
           </table>
         </div>
         <div className="join flex flex-row justify-center items-center pt-2">
-          <button className="join-item btn btn-sm btn-square btn-secondary">
+          <button
+            className="join-item btn btn-sm btn-square btn-neutral"
+            onClick={() => handleNavigation(false)}
+            disabled={page === 1}
+          >
             «
           </button>
-          <button className="join-item btn btn-sm btn-square btn-secondary">
-            1
+          <button className="join-item btn btn-sm btn-square btn-neutral">
+            {page}
           </button>
-          <button className="join-item btn btn-sm btn-square btn-secondary">
+          <button
+            className="join-item btn btn-sm btn-square btn-neutral"
+            onClick={() => handleNavigation(true)}
+            disabled={page * ITEMS_PER_PAGE >= data.length}
+          >
             »
           </button>
         </div>
