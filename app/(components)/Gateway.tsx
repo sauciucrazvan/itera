56840d03520f2b096d6e95c85bb4695a859a5a66
Razvan <razvansauciucc@outlook.com>
2024-09-10 18:45:21 +0300

    Added the gateway: a component that checks if the user is logged in before accessing a page

---
+++ app/(components)/Gateway.tsx
@@ -0,0 +1,31 @@
+/*
+    Gateway is a component that checks if the user is logged in and returns the page,
+    otherwise it will redirect to the login page.
+*/
+
+"use client";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import { ReactNode } from "react";
+import { useRouter } from "next/navigation";
+import Loading from "./Loading";
+
+interface GatewayProps {
+  children: ReactNode;
+}
+
+export default function Gateway({ children }: GatewayProps) {
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (!user) {
+    router.push("/login");
+    return <div className="alert alert-error">You need to be logged in!</div>;
+  }
+
+  return <>{user && children}</>;
+}
--- app/thread/new/page.tsx
+++ app/thread/new/page.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { isSeverity, Severity, severityTypes } from "@/app/(types)/Severities";
 import { insertThread } from "@/app/(database)/addThread";
 import { useRouter } from "next/navigation";
+import Gateway from "@/app/(components)/Gateway";
 
 export default function NewIssue() {
   const [title, setTitle] = useState("");
@@ -43,75 +44,77 @@ export default function NewIssue() {
 
   return (
     <>
-      <section className="py-2 px-4">
-        <div className="breadcrumbs text-sm">
-          <ul>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/">Issues</Link>
-            </li>
-            <li>New Thread</li>
-          </ul>
-        </div>
-        <section className="artboard bg-base-200 px-4 py-2 rounded-md">
-          <h1 className="font-bold text-lg">Create a thread</h1>
-          <div className="divider m-0" />
-          <section className="pt-2 flex flex-col lg:flex-row lg:items-center gap-1">
-            <div>
-              <h1>Add a title</h1>
-              <input
-                type="text"
-                placeholder="Title"
-                className="input input-bordered w-full max-w-xs"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </div>
-            <div>
-              <h1>Severity</h1>
+      <Gateway>
+        <section className="py-2 px-4">
+          <div className="breadcrumbs text-sm">
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/">Issues</Link>
+              </li>
+              <li>New Thread</li>
+            </ul>
+          </div>
+          <section className="artboard bg-base-200 px-4 py-2 rounded-md">
+            <h1 className="font-bold text-lg">Create a thread</h1>
+            <div className="divider m-0" />
+            <section className="pt-2 flex flex-col lg:flex-row lg:items-center gap-1">
+              <div>
+                <h1>Add a title</h1>
+                <input
+                  type="text"
+                  placeholder="Title"
+                  className="input input-bordered w-full max-w-xs"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div>
+                <h1>Severity</h1>
 
-              <select
-                className="select select-bordered w-full max-w-xs"
-                onChange={(event) =>
-                  setSeverity(event.target.value as Severity)
-                }
-                value={severity}
-              >
-                {Object.entries(severityTypes).map(([key, value]) => (
-                  <option key={key} value={key} className={value}>
-                    {key}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </section>
-          <section className="py-2 flex flex-col">
-            <h1>Add a description</h1>
-            <small className="pb-2">
-              Please be as descriptive as possible.
-              <br />
-              Upload images to trusted websites (such as{" "}
-              <Link href="https://imgur.com">imgur.com</Link>)
-            </small>
-            <textarea
-              className="
+                <select
+                  className="select select-bordered w-full max-w-xs"
+                  onChange={(event) =>
+                    setSeverity(event.target.value as Severity)
+                  }
+                  value={severity}
+                >
+                  {Object.entries(severityTypes).map(([key, value]) => (
+                    <option key={key} value={key} className={value}>
+                      {key}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </section>
+            <section className="py-2 flex flex-col">
+              <h1>Add a description</h1>
+              <small className="pb-2">
+                Please be as descriptive as possible.
+                <br />
+                Upload images to trusted websites (such as{" "}
+                <Link href="https://imgur.com">imgur.com</Link>)
+              </small>
+              <textarea
+                className="
             textarea
             textarea-bordered
             textarea-md
             w-screen
             max-w-md"
-              placeholder="Describe your issue..."
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
+                placeholder="Describe your issue..."
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </section>
+            <button className="btn btn-success btn-xs" onClick={addThread}>
+              Submit new issue
+            </button>
           </section>
-          <button className="btn btn-success btn-xs" onClick={addThread}>
-            Submit new issue
-          </button>
         </section>
-      </section>
+      </Gateway>
     </>
   );
 }
